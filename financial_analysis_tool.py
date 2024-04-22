7d4a3a1f208f420ae08b69a1f2bcf8af10ee10b9
CameronH78 <162645462+CameronH78@users.noreply.github.com>
2024-04-21 18:20:28 -0600

    Create financial_analysis_tool.py
    Project file

---
+++ financial_analysis_tool.py
@@ -0,0 +1,90 @@
+import tkinter as tk
+from tkinter import ttk
+from datetime import datetime
+import yfinance as yf
+from matplotlib import pyplot as plot
+import seaborn as sns
+
+
+# Processing but needs more maybe
+def data_process(data):
+    data.fillna(data.mean(), inplace=True)
+    return data
+
+
+def make_graph():
+    symbol = symbol_set.get()
+    # Google Graph
+    if symbol == "GOOGL":
+        start_date = start_date_entry.get()
+        end_date = end_date_entry.get()
+
+        # Collect the data from yfinance module using a symbol
+        data = yf.download(symbol, start=start_date, end=end_date)
+
+        # Visualize data by creating the graph
+        plot.figure(figsize=(12, 6))
+        sns.lineplot(x=data.index, y=data['Close'])
+        plot.title(f'Stock Prices for GOOGLE')
+        plot.xlabel('Date')
+        plot.ylabel('Price')
+        plot.show()
+
+    # Amazon Graph
+    elif symbol == "AMZN":
+        start_date = start_date_entry.get()
+        end_date = end_date_entry.get()
+
+        # Collect the data from yfinance module using a symbol
+        data = yf.download(symbol, start=start_date, end=end_date)
+
+        # Visualize data by creating the graph
+        plot.figure(figsize=(12, 6))
+        sns.lineplot(x=data.index, y=data['Close'])
+        plot.title(f'Stock Prices for AMAZON')
+        plot.xlabel('Date')
+        plot.ylabel('Price')
+        plot.show()
+    # Microsoft Graph goes here.
+
+
+# Clears the entry boxes and will use a button
+# def reset_boxes():
+#    start_date_entry.delete(0, 'end')
+#    end_date_entry.delete(0, 'end')
+#    symbol_set.delete(0, 'end')
+
+
+# Create main window
+graph = tk.Tk()
+graph.title("Test ICT-372 Graph")
+
+# Create and place labels and talk boxes
+start_date = ttk.Label(graph, text="Start at - (YYYY-MM-DD):")
+start_date.grid(row=0, column=0, padx=10, pady=5)
+start_date_entry = ttk.Entry(graph, width=20)
+start_date_entry.grid(row=0, column=1, padx=10, pady=5)
+start_date_entry.insert(0, '')
+
+end_date = ttk.Label(graph, text="End at - (YYYY-MM-DD):")
+end_date.grid(row=1, column=0, padx=10, pady=5)
+end_date_entry = ttk.Entry(graph, width=20)
+end_date_entry.grid(row=1, column=1, padx=10, pady=5)
+end_date_entry.insert(0, '')
+
+# Labels - Google = GOOGL, Microsoft = MSFT, Amazon = AMZN
+symbol_label = ttk.Label(graph, text=" Pick a stock:\n GOOGL\n AMZN\n MSFT(Not finished)")
+symbol_label.grid(row=2, column=0, padx=10, pady=5)
+symbol_set = ttk.Entry(graph, width=20)
+symbol_set.grid(row=2, column=1, padx=10, pady=5)
+
+# Creates the graph with the start button
+start_button = ttk.Button(graph, text="TEST BUTTON", command=make_graph)
+start_button.grid(row=3, column=0, columnspan=2, padx=20, pady=20)
+
+# Clears all the boxes with a reset button
+# reset_button = ttk.Button(graph, text="TEST RESET", command=reset_boxes)
+# reset_button.grid(row=3, column=1, padx=5, pady=20)
+
+# Start the Tkinter event loop
+graph.mainloop()
